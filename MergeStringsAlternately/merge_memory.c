01490fe03eb6b1ebb43edf033115a12d78441a6a
Alexandre Gaasch <alexandre.gaasch@gmail.com>
2026-05-01 10:46:22 +0200

    slight improvement

--- MergeStringsAlternately/merge_memory.c
+++ MergeStringsAlternately/merge_memory.c
@@ -2,13 +2,13 @@
 // My solution to get a bit better memory usage. 100% runtime (0ms) and 75% memory 8.6 MB
 
 char* mergeAlternately(const char* word1, const char* word2) {
-    int len1 = strlen(word1);
-    int len2 = strlen(word2);
+    size_t len1 = strlen(word1);
+    size_t len2 = strlen(word2);
     
-    char* result = (char*)malloc(len1 + len2 + 1);
+    char* result = malloc(len1 + len2 + 1);
     if (!result) return NULL;
 
-    int i = 0, j = 0, k = 0;
+    size_t i = 0, j = 0, k = 0;
 
     while (i < len1 && j < len2) {
         result[k++] = word1[i++];
